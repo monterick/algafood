695402da29f5d0b00a9fcfcc9855f5a6febf8776
monterick <rick_-26@hotmail.com>
2023-01-09 20:14:50 -0300

    'commit'

---
+++ algafood-algaworks/src/main/java/com/algaworks/algafood/AlgafoodAlgaworksApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgafoodAlgaworksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgafoodAlgaworksApplication.class, args);
+	}
+
+}
