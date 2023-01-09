24fa7845f9e5d6fdb30000a949bd33559d8073ff
monterick <rick_-26@hotmail.com>
2023-01-09 10:04:36 -0300

    'commit'

--- algafood-algaworks/src/main/resources/import.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO cozinha(id, nome) VALUES(1, 'Indiana');
-INSERT INTO cozinha(id, nome) VALUES(2, 'Asiatica');
-
-INSERT INTO restaurante(nome,cozinha_id) VALUES( 'delivery', 1);
-INSERT INTO restaurante(nome,cozinha_id) VALUES( 'Jimc', 2);
