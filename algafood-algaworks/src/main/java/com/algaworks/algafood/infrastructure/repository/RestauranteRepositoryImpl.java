452276c155912317dac136da15bec787616176b4
monterick <rick_-26@hotmail.com>
2023-01-11 12:12:28 -0300

    'restaurantes'

---
+++ algafood-algaworks/src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -0,0 +1,47 @@
+package com.algaworks.algafood.infrastructure.repository;
+
+import java.util.List;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+
+@Component
+public class RestauranteRepositoryImpl implements RestauranteRepository{
+
+	@PersistenceContext
+	EntityManager manager;
+	
+	@Override
+	public List<Restaurante> listarRestaurantes() {
+		return manager.createQuery("from Restaurante", Restaurante.class).getResultList();
+	}
+
+	@Override
+	public Restaurante buscarRestaurante(long id) {
+		return manager.find(Restaurante.class, id);
+	}
+
+	@Override
+	@Transactional
+	public Restaurante adicionarRestaurante(Restaurante restaurante) {
+		return manager.merge(restaurante);
+	}
+
+	@Override
+	@Transactional		
+	public void removerRestaurante(long id) {		
+		Restaurante restaurante = buscarRestaurante(id);
+		if(restaurante == null) {
+			throw new EmptyResultDataAccessException(1);
+		}
+		manager.remove(restaurante);
+	}
+
+}
